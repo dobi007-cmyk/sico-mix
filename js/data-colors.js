7a092476c662b7d2effcc2e10d54a7f5ac92c612
dobi007-cmyk <mamleevborya@gmail.com>
2026-01-30 09:07:32 +0100

    Add color data to data-colors.js

---
+++ js/data-colors.js
@@ -0,0 +1,10 @@
+const COLORS = [
+  { code: "EC10", name: "Blue violet", hex: "#3b3f9f" },
+  { code: "EC20", name: "Dark blue", hex: "#1f3b7a" },
+  { code: "EC20/B", name: "Reflex blue", hex: "#002b7f" },
+  { code: "EC30", name: "Dark green", hex: "#0f4d2f" },
+  { code: "EC40", name: "Yellow", hex: "#ffd400" },
+  { code: "EC50", name: "Orange", hex: "#ff7a00" },
+  { code: "EC60", name: "Red", hex: "#c4001a" },
+  { code: "EC70", name: "Magenta", hex: "#d1007a" },
+  { code: "EC100", name: "Black", hex: "#000000" }
