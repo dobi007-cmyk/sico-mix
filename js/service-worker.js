70a1b30c6c5486a2cbd04001b7fc23b04d77b3b4
dobi007-cmyk <mamleevborya@gmail.com>
2026-02-06 14:14:18 +0100

    Update service-worker.js

--- js/service-worker.js
+++ js/service-worker.js
@@ -11,15 +11,18 @@ const urlsToCache = [
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
+// Install service worker
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => {
+                console.log('Cache otwarty');
                 return cache.addAll(urlsToCache);
             })
     );
 });
 
+// Fetch from cache first, then network
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
@@ -32,12 +35,14 @@ self.addEventListener('fetch', event => {
     );
 });
 
+// Clean up old caches
 self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys().then(cacheNames => {
             return Promise.all(
                 cacheNames.map(cacheName => {
                     if (cacheName !== CACHE_NAME) {
+                        console.log('Usuwanie starego cache:', cacheName);
                         return caches.delete(cacheName);
                     }
                 })
